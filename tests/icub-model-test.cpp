82b5cf5f4504e882ffd30b9ae7370701b63d3757
Silvio <pegua1@gmail.com>
2016-08-09 11:42:11 +0200

    Added test on soles simmetry

--- tests/icub-model-test.cpp
+++ tests/icub-model-test.cpp
@@ -178,6 +178,7 @@ bool checkSolesAreParallel(iDynTree::KinDynComputations & comp)
     iDynTree::Transform root_H_l_sole = comp.getRelativeTransform(rootLinkIdx,l_sole);
     iDynTree::Transform root_H_r_sole = comp.getRelativeTransform(rootLinkIdx,r_sole);
 
+    // height of the sole should be equal
     double l_sole_height = root_H_l_sole.getPosition().getVal(2);
     double r_sole_height = root_H_r_sole.getPosition().getVal(2);
 
@@ -186,6 +187,27 @@ bool checkSolesAreParallel(iDynTree::KinDynComputations & comp)
         std::cerr << "icub-model-test error: l_sole_height is " << l_sole_height << ", while r_sole_height is " << r_sole_height << " (diff : " << std::fabs(l_sole_height-r_sole_height) <<  " )"  << std::endl;
         return false;
     }
+
+    // x should also be equal
+    double l_sole_x = root_H_l_sole.getPosition().getVal(0);
+    double r_sole_x = root_H_r_sole.getPosition().getVal(0);
+
+    if( !checkDoubleAreEqual(l_sole_x,r_sole_x,1e-5) )
+    {
+        std::cerr << "icub-model-test error: l_sole_x is " << l_sole_x << ", while r_sole_x is " << r_sole_x << " (diff : " << std::fabs(l_sole_x-r_sole_x) <<  " )"  << std::endl;
+        return false;
+    }
+
+    // y should be simmetric
+    double l_sole_y = root_H_l_sole.getPosition().getVal(1);
+    double r_sole_y = root_H_r_sole.getPosition().getVal(1);
+
+    if( !checkDoubleAreEqual(l_sole_y,-r_sole_y,1e-5) )
+    {
+        std::cerr << "icub-model-test error: l_sole_y is " << l_sole_y << ", while r_sole_y is " << r_sole_y << " while they should be simmetric (diff : " << std::fabs(l_sole_y+r_sole_y) <<  " )"  << std::endl;
+        return false;
+    }
+
 
     std::cerr << "icub-model-test error: sole are parallel test performed correctly " << std::endl;
 
