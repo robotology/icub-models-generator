dabd98556541617516c6f022d4fdf9225e0efd91
Nicolò <nicolo.genesio@iit.it>
2020-09-01 17:27:45 +0200

    Add limits for upperbody

--- simmechanics/data/icub3/convertModel.sh
+++ simmechanics/data/icub3/convertModel.sh
@@ -1,2 +1,2 @@
-simmechanics_to_urdf SIM_ICUB3_UPPERBODY.xml --yaml icub3-upperbody-options.yaml --outputfile model.urdf
+ simmechanics_to_urdf SIM_ICUB3_UPPERBODY.xml  --csv ICUB_3_joint_upperbody_parameters.csv --yaml ICUB_3_upperbody_options.yaml --outputfile model.urdf
 gz sdf -p model.urdf > iCub3Upperbody.sdf
