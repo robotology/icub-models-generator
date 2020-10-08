c0e286765e0fba1c9d0b5d5d99c2446f26cbd749
Nicolò <nicolo.genesio@iit.it>
2020-10-08 16:43:58 +0200

    icub3: add all left leg files

--- simmechanics/data/icub3/convertModel.sh
+++ simmechanics/data/icub3/convertModel.sh
@@ -1,2 +1,6 @@
  simmechanics_to_urdf ICUB3_UPPERBODY_SIM_MODEL.xml  --csv ICUB_3_joint_upperbody_parameters.csv --yaml ICUB_3_upperbody_options.yaml --outputfile model.urdf
 gz sdf -p model.urdf > iCub3Upperbody.sdf
+
+
+simmechanics_to_urdf ICUB3_L_LEG_SIM_MODEL.xml  --csv ICUB_3_joint_leftleg_parameters.csv --yaml ICUB_3_leftleg_options.yaml --outputfile model.urdf
+gz sdf -p model.urdf > iCub3LeftLeg.sdf
