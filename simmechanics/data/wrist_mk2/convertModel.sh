0e63c7958e825d81138b1088877f158667590d00
ale-git <alessandro.scalzo@iit.it>
2021-05-12 08:58:59 +0200

    Added wrist model, fixed generation errors

--- simmechanics/data/wrist_mk2/convertModel.sh
+++ simmechanics/data/wrist_mk2/convertModel.sh
@@ -1,2 +1,2 @@
 simmechanics_to_urdf SIM_L_WRIST.xml --yaml simmechanics2urdf_configfile.yaml --csv-joints simmechanics2urdf_joints_configfile.csv --output xml --outputfile left_wrist_mk2.urdf
-#gz sdf -p left_wrist_mk2.urdf > left_wrist_mk2.sdf
+gz sdf -p left_wrist_mk2.urdf > left_wrist_mk2.sdf
